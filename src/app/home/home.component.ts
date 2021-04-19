553f4ce71decff43a70460829e75452ed47970ca
Mohit Sethi <mohit.mohit.sethi@gmail.com>
2021-04-19 21:17:39 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -169,7 +169,7 @@ export class HomeComponent implements OnInit {
 
     }
     console.log(body)
-    fetch('http://127.0.0.1:8080/login',{
+    fetch('https://save2rent.com/app2021/api/public',{
       method:'POST',
       body:formData,
 
